51f26d1e62dafc51389aada18e0de5dc3494ebd6
Lorenzo Monacelli <mesonepigreco@gmail.com>
2024-03-07 22:15:27 +0100

    Now it works and shows also the electric field

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ import { Box } from './shape.js';
 import {Conductor} from './conductor.js';
 import {start_dragging, move_objects_on_drag, stop_dragging} from './drag_and_drop.js';
 import {poisson_one_step, get_electric_field} from './solver.js';
+import {draw_efield} from './draw_efield.js';
 
 
 // Integrate the poisson law
-const dx = 0.01;
+const Delta = 10;
 
 const list_of_items = [];
 
@@ -28,7 +29,9 @@ clear_button.addEventListener("click", function() {
 	clear_boxes();
 });
 
-
+const potentialLabel = document.getElementById("mouse-potential");
+const exLabel = document.getElementById("mouse-field-x");
+const eyLabel = document.getElementById("mouse-field-y");
 
 
 
@@ -39,7 +42,7 @@ function update_potential(value) {
 	// Set the potential of the selected objects
 	for (let i = 0; i < list_of_items.length; i++) {
 		if (list_of_items[i].shape.selected) {
-			list_of_items[i].potential = value;
+			list_of_items[i].potential = parseFloat(value);
 		}
 	}
 }
@@ -85,6 +88,11 @@ canvas.addEventListener("mousemove", function(e) {
 	let mouseX = e.clientX - canvas.getBoundingClientRect().left;
 	let mouseY = e.clientY - canvas.getBoundingClientRect().top;
 	move_objects_on_drag(list_of_items, mouseX, mouseY);
+
+	// Update the value of the potential in the mouse-potential object
+	potentialLabel.innerHTML = V_array[mouseX][mouseY];
+	exLabel.innerHTML = efield_x[mouseX][mouseY] * 100;
+	eyLabel.innerHTML = efield_y[mouseX][mouseY] * 100;
 });
 canvas.addEventListener("mouseup", function(e) {
 	stop_dragging(list_of_items);
@@ -98,16 +106,22 @@ let V_array = new Array(Nx);
 let fixed_mask = new Array(Nx);
 let charge_density = new Array(Nx);
 let V_tmp = new Array(Nx);
+let efield_x = new Array(parseInt(Nx / Delta));
+let efield_y = new Array(parseInt(Nx / Delta));
 for (let i = 0; i < Nx; i++) {
 	V_array[i] = new Array(Ny);
 	fixed_mask[i] = new Array(Ny);
 	charge_density[i] = new Array(Ny);
 	V_tmp[i] = new Array(Ny);
+	efield_x[i] = new Array(parseInt(Ny / Delta));
+	efield_y[i] = new Array(parseInt(Ny / Delta));
 	for (let j = 0; j < Ny; j++) {
 		V_array[i][j] = 0;
 		fixed_mask[i][j] = 0;
 		charge_density[i][j] = 0;
 		V_tmp[i][j] = 0;
+		efield_x[i][j] = 0;
+		efield_y[i][j] = 0;
 	}
 }
 
@@ -117,18 +131,31 @@ function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	draw_all(ctx);
 
+	// Draw the electric field	
+	draw_efield(ctx, efield_x, efield_y, Delta, 1000);
+	
+
 	requestAnimationFrame(animate);
 
 
 	// Solve the poissons equation
 	if (solve_this_step) {
+		// Reset the fixed mask
+		for (let i = 0; i < Nx; i++) {
+			for (let j = 0; j < Ny; j++) {
+				fixed_mask[i][j] = 0;
+			}
+		}
 		// Apply the constrains on the potential
 		for (let i = 0; i < list_of_items.length; i++) {
-			list_of_items[i].applyFixedPotential(V_array, dx, fixed_mask);
+			list_of_items[i].applyFixedPotential(V_array, 1.0, fixed_mask);
 		}
 
 		// Solve the poisson equation
 		poisson_one_step(V_array, fixed_mask, charge_density, V_tmp);
+
+		// Get the electric field
+		get_electric_field(V_array, Delta, efield_x, efield_y);
 	}
 
 }
--- solver.js
+++ solver.js
@@ -11,15 +11,28 @@ export function poisson_one_step(V_array, fixed_mask, charge_density, V_new) {
 
 	for (let i = 0; i < Nx; i++) {
 		for (let j = 0; j < Ny; j++) {
-			if (fixed_mask[i][j] == 0) {
-				// Apply the periodic boundary conditions
-				let i_prev = (i - 1 + Nx) % Nx;
-				let i_next = (i + 1) % Nx;
-				let j_prev = (j - 1 + Ny) % Ny;
-				let j_next = (j + 1) % Ny;
+			// Check if this is not NaN
+			if (!isNaN(V_array[i][j])) {
+				if (fixed_mask[i][j] == 0) {
+					// Apply the periodic boundary conditions
+					let i_prev = (i - 1 + Nx) % Nx;
+					let i_next = (i + 1) % Nx;
+					let j_prev = (j - 1 + Ny) % Ny;
+					let j_next = (j + 1) % Ny;
 
-				// Integrate the Poisson law
-				V_new[i][j] = 0.25 * (V_array[i_prev][j] + V_array[i_next][j] + V_array[i][j_prev] + V_array[i][j_next] + charge_density[i][j]);
+					// Integrate the Poisson law
+					V_new[i][j] = 0.25 * (V_array[i_prev][j] + V_array[i_next][j] + V_array[i][j_prev] + V_array[i][j_next]) + charge_density[i][j];
+
+					if (isNaN(V_new[i][j])) {
+						console.log("NaN detected at i = " + i + " j = " + j);
+						console.log("i_prev = " + i_prev + " i_next = " + i_next + " j_prev = " + j_prev + " j_next = " + j_next);
+						console.log(V_array[i_prev][j]);
+						console.log(V_array[i_next][j]);
+						console.log(V_array[i][j_prev]);
+						console.log(V_array[i][j_next]);
+						console.log(charge_density[i][j]);
+					}
+				}
 			}
 		}
 	}
@@ -44,33 +57,22 @@ export function poisson_one_step(V_array, fixed_mask, charge_density, V_new) {
  * @param {Number} Delta - The resolution for the electric field
  * @return {Array} [E_x, E_y] - The electric field (2D) on a grid of Delta resolution
  */
-export function get_electric_field(V_array, Delta) {
+export function get_electric_field(V_array, Delta, E_x, E_y) {
 	let Nx = V_array.length;
 	let Ny = V_array[0].length;
 
-	E_x = [];
-	E_y = [];
 
 	for (let i = 0; i < Nx; i+=Delta) {
-		E_x.push([]);
-		E_y.push([]);
 		for (let j = 0; j < Ny; j+=Delta) {
-			i_prev = (i - Delta + Nx) % Nx;
-			i_next = (i + Delta) % Nx;
-			j_prev = (j - Delta + Ny) % Ny;
-			j_next = (j + Delta) % Ny;
+			let i_prev = (i - Delta + Nx) % Nx;
+			let i_next = (i + Delta) % Nx;
+			let j_prev = (j - Delta + Ny) % Ny;
+			let j_next = (j + Delta) % Ny;
 
 			// Compute the gradient of the potential
-			dV_dx = (V_array[i_next][j] - V_array[i_prev][j]) / (2 * Delta * dx);
-			dV_dy = (V_array[i][j_next] - V_array[i][j_prev]) / (2 * Delta * dx);
-
-			// Compute the electric field
-			E_x[i].push(-dV_dx);
-			E_y[i].push(-dV_dy);
+			E_x[i][j] = - (V_array[i_next][j] - V_array[i_prev][j]) / (2 * Delta);
+			E_y[i][j] = -(V_array[i][j_next] - V_array[i][j_prev]) / (2 * Delta);
 		}
 	}
-
-	return [E_x, E_y];
 }
-
 
